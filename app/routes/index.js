3202db4cab97c949105314c84d10e20079f1b40b
tobensg <tobensg@gmail.com>
2016-04-04 12:02:15 -0700

    thinking of switching gears to really simple

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var keys = require('../keys');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log('serving get');
+  res.render('index', { key: keys.DarkSkyForecastAPI });
 });
 
 module.exports = router;
